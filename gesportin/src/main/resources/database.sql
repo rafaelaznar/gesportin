d21c5998ca32869e2a3dd72463eef7a187db7c87
Lucia Castanera <castaneralucia@gmail.com>
2026-01-08 09:11:36 +0100

    TABLA FACTURA - LUCIA

--- gesportin/src/main/resources/database.sql
+++ gesportin/src/main/resources/database.sql
@@ -7,28 +7,8 @@
 --
 
 CREATE TABLE `factura` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `fecha` datetime NOT NULL,
-  `id_usuario` bigint NOT NULL
+  `id_usuario` bigint NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `factura`
---
-ALTER TABLE `factura`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
-COMMIT;
