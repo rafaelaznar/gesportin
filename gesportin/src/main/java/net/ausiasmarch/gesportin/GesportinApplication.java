84245d1a9061421bc1ec2b0f7ad3c4a6961dd705
Rafael Aznar <rafaaznar@gmail.com>
2026-01-08 08:41:46 +0100

    Commit inicial

---
+++ gesportin/src/main/java/net/ausiasmarch/gesportin/GesportinApplication.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.gesportin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesportinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesportinApplication.class, args);
+	}
+
+}
---
+++ gesportin/src/test/java/net/ausiasmarch/gesportin/GesportinApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.gesportin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesportinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
