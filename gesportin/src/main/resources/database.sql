c2a218054a108a6fdbd0e8fc51a909826a1b93e7
Alejandro Pavon <alejandropm06@gmail.com>
2026-01-08 08:58:53 +0100

    Añadir mi nombre

--- gesportin/src/main/resources/database.sql
+++ gesportin/src/main/resources/database.sql
@@ -1,5 +1,24 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
 --
--- Estructura de tabla para la tabla `temporada` (Alejandro Pavón Martínez)
+-- Servidor: database:3306
+-- Tiempo de generación: 08-01-2026 a las 07:56:16
+-- Versión del servidor: 8.4.6
+-- Versión de PHP: 8.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Base de datos: `club`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `temporada`
 --
 
 CREATE TABLE `temporada` (
