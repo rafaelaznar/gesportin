31c03393024352243f67c2d033a4977afa430127
PollySOA <pollysoares.dev@gmail.com>
2026-02-04 10:39:17 +0100

    Initial implementation of ViewRouted component with basic structure and template setup

---
+++ frontsportin/src/app/component/puntuacion/view-routed/puntuacion-view.ts
@@ -0,0 +1,48 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { PuntuacionService } from '../../../service/puntuacion';
+import { IPuntuacion } from '../../../model/puntuacion';
+
+@Component({
+  selector: 'app-view-routed',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './puntuacion-view.html',
+  styleUrl: './puntuacion-view.css',
+})
+export class PuntuacionViewRouted implements OnInit {
+  private route = inject(ActivatedRoute);
+  private oPuntuacionService = inject(PuntuacionService);
+
+oPuntuacion = signal<IPuntuacion | null>(null);
+loading = signal(true);
+error = signal<string | null>(null);
+
+  ngOnInit(): void {
+   const idParam = this.route.snapshot.paramMap.get('id');
+  const id = idParam ? Number(idParam) : NaN;
+  if (isNaN(id)) {
+    this.error.set('ID no válido');
+    this.loading.set(false);
+    return;
+  }
+  this.loading(id);
+  }
+
+  load(id: number) {
+    this.oPuntuacionService.get(id).subscribe({
+      next: (puntuacion) => {
+        this.oPuntuacion.set(puntuacion);
+        this.loading.set(false);
+      },
+      error: (err: HttpErrorResponse) => {
+        this.error.set('Error al cargar la puntuación');
+        this.loading.set(false);
+
+        console.error(err);
+      },
+    });
+  }
+
+}
